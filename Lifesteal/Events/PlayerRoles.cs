ef9191ec8a1121820e642501f87fecb3c4717fa3
DasIschBims <dasischbims@gmail.com>
2023-11-29 10:08:44 +0100

    improve giving role, only give role to me lol

--- Lifesteal/Events/PlayerRoles.cs
+++ Lifesteal/Events/PlayerRoles.cs
@@ -6,12 +6,11 @@ public class PlayerRoles : Event
 {
     public override Task OnPlayerConnected(LifestealPlayer player)
     {
-        if (player.SteamID == 76561198395073327) return Task.CompletedTask;
-        
-        if (player.AddPlayerRole(Enums.PlayerRoles.Admin))
-        {
-            Program.Logger.Info($"Successfully added roles for {player.Name} ({player.SteamID})");
-        }
+        if (player.SteamID != 76561198395073327) return Task.CompletedTask;
+
+        Program.Logger.Info(player.AddPlayerRole(Enums.PlayerRoles.Admin)
+            ? $"Successfully added roles for {player.Name} ({player.SteamID})"
+            : $"User {player.Name} ({player.SteamID}) already has the Role.");
 
         return Task.CompletedTask;
     }
